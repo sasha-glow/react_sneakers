cdc16b957179f1b039cb87e0e2c04dc06525e74e
Sasha_Glow <sasha_glow@yandex.ru>
2025-07-09 18:09:35 +0200

    finished lesson 3 react-sneakers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede', 
+    price: 12999,
+    imageUrl: 'img/sneakers/01.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270', 
+    price: 15999,
+    imageUrl: 'img/sneakers/02.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede', 
+    price: 8499,
+    imageUrl: 'img/sneakers/03.jpg'
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider', 
+    price: 9999,
+    imageUrl: 'img/sneakers/04.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Under Armour Curry 8', 
+    price: 14299,
+    imageUrl: 'img/sneakers/05.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Kyrie 7', 
+    price: 11399,
+    imageUrl: 'img/sneakers/06.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Jordan Air Jordan 11', 
+    price: 10799,
+    imageUrl: 'img/sneakers/07.jpg'
+  },
+  
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -15,12 +54,16 @@ function App() {
             <input type="text" placeholder="Поиск..." />
           </div>
         </div>
-        
+
         <div className="d-flex cards">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card 
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
